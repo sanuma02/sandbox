99e2aaf75b008adecf36fb3e9a8ae6fbbb435265
fanny-umana <fanny.umana@netbraintech.com>
2022-02-01 16:07:13 +0100

    init

---
+++ sae_backend/app.py
@@ -0,0 +1,4 @@
+from sae import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ sae_backend/config.py
@@ -0,0 +1,6 @@
+CLIENT = "localhost"
+CLIENT_PORT = 27017
+CLIENT_USER = "main_admin"
+CLIENT_PASS = ""
+DB = "SAE"
+SERVICE_CO = "service"
